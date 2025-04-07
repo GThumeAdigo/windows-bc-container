96d84a2f99b17391b0343ba35e6f19dd80ed29b5
GThumeAdigo <gregor.thume@adigo.eu>
2025-04-07 10:14:58 +0200

    new iteration, trying to get hyper v to work

--- startup-scripts/install.bat
+++ startup-scripts/install.bat
@@ -7,9 +7,6 @@ echo Installing NuGet and BcContainerHelper...
 powershell -Command "Install-PackageProvider -Name NuGet -Force"
 powershell -Command "Install-Module -Name BcContainerHelper -Force"
 
-echo Install Hyper-V...
-powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart"
-
 echo Installing Docker...
 powershell -Command "Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1' -o install-docker-ce.ps1"
 powershell -Command ".\install-docker-ce.ps1"
