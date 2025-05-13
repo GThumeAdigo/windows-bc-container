04dc78183631285a484de696a3a51cb872e40980
GThumeAdigo <gregor.thume@adigo.eu>
2025-05-13 13:38:16 +0200

    New arguments for windows container for nested virtualisation

--- startup-scripts/createcontainer.ps1
+++ startup-scripts/createcontainer.ps1
@@ -1,3 +1,3 @@
 $url = Get-BCArtifactUrl -Select Latest -Type OnPrem -Country de
 $cred = Get-Credential
-New-BcContainer -accept_eula -artifactUrl $url -Credential $cred -auth UserPassword -PublishPorts @(443,8080,80,7048,7047,7049) -isolation hyperv
+New-BcContainer -accept_eula -artifactUrl $url -Credential $cred -auth UserPassword -PublishPorts @(443,8080,80,7048,7047,7049)  -updateHosts -useBestContainerOS -alwaysPull 
--- startup-scripts/install.bat
+++ startup-scripts/install.bat
@@ -3,9 +3,17 @@ echo ========================================
 echo Setup for BC Container 
 echo ========================================
 
-echo Installing NuGet and BcContainerHelper...
+echo Disabling Windows Defender...
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+echo Successfully disabled Windows Defender
+
+echo Installing NuGet
 powershell -Command "Install-PackageProvider -Name NuGet -Force"
+echo Successfully installed NuGet
+
+echo Installing BcContainerHelper
 powershell -Command "Install-Module -Name BcContainerHelper -Force"
+echo Successfully installed BcContainerHelper
 
 echo Installing Docker...
 powershell -Command "Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-DockerCE/install-docker-ce.ps1' -o install-docker-ce.ps1"
