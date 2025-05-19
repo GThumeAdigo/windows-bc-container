39e57477958b95d25dfe577058e84a415741e1ae
GThumeAdigo <gregor.thume@adigo.eu>
2025-05-19 11:51:18 +0200

    excluding standard microfoft test apps, including performance toolkit

--- startup-scripts/createcontainer.ps1
+++ startup-scripts/createcontainer.ps1
@@ -1,4 +1,4 @@
 $url = Get-BCArtifactUrl -Select Latest -Type OnPrem -Country de
 $cred = New-Object pscredential 'adigo', (ConvertTo-SecureString -String 'adigo' -AsPlainText -Force)
 
-New-BcContainer -accept_eula -artifactUrl $url -Credential $cred -auth UserPassword -PublishPorts @(443,8080,80,7048,7047,7049)  -updateHosts -useBestContainerOS -alwaysPull -includeTestToolkit 
+New-BcContainer -accept_eula -artifactUrl $url -Credential $cred -auth UserPassword -PublishPorts @(443,8080,80,7048,7047,7049)  -updateHosts -useBestContainerOS -alwaysPull -includeTestToolkit -includeTestLibrariesOnly -includePerformanceToolkit
